4b5bb2738bfaa4471b1b85641c41a8b13d8ca9c9
Amy Young <amy.kristen.young@gmail.com>
2019-09-08 14:48:10 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 def using_while
 
+=======
+>>>>>>> 19f171651f21c60ebf36dacf71dc55e0117f3f14
   levitation_force = 6
 	while levitation_force < 10
     puts "Wingardium Leviosa"
     levitation_force += 1
   end
+<<<<<<< HEAD
 end
+=======
+>>>>>>> 19f171651f21c60ebf36dacf71dc55e0117f3f14
